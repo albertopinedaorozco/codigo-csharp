045c94ec21109cf6f2a8b496f171ba7ee9adcf00
Alberto PIneda Orozco <ing.firemann@gmail.com>
2019-09-26 09:08:10 -0500

    Ejemplo proyecto en c#

---
+++ Ejemplo2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: CBN
+ * Fecha: 26/09/2019
+ * Hora: 09:06 a.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Ejemplo2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
